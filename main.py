89aab3faedf84aba57051f46ef25caebcf7f4fc4
McKennon McMillian <mbmcmill@go.olemiss.edu>
2017-11-05 22:47:01 -0600

    Main renders models with no light

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ test = p("model0.ply")
 test.parse()
 vl = test.vert_list
 fl = test.face_list
-print('vl ',len(vl), vl[2])
-print('fl ',len(fl), fl[2])
+print('vl ',len(vl), vl[0], vl[len(vl)-1])
+print('fl ',len(fl), fl[0], fl[len(fl)-1])
 # print('test ', vl[2][fl[2][3]])
 class GLContext():
 	def __init__(self, screen):
+		glEnable(GL_DEPTH_TEST)
+		glDepthFunc(GL_LEQUAL)
 		self.screen=screen
+		self.aspect = screen.get_width()/screen.get_height()
+		gluPerspective(45.0, self.aspect, 0.1, 200.0)
 
 		glClearColor(0.0, 0.0, 0.0, 1.0)
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -34,17 +38,21 @@ class GLContext():
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 		glPushMatrix()
+		glTranslatef(0.0, 0.0, -5.0) 
+		glRotatef(45.0, 1, 0, 0)  
+		glRotatef(15, 0, 1, 0) 
+		glRotatef(0.0, 0, 0, 1) 
+		
 		glBegin(GL_TRIANGLES)
-
+		
 		glColor3f(1.0, 0.0, 1.0)
-		for x in range(len(fl)): #x starts at 1
-			print(x)
-			glVertex2f(vl[fl[x][1]], vl[fl[x][2]])
-			glVertex2f(vl[fl[x][2]], vl[fl[x][3]])
-			glVertex2f(vl[fl[x][3]], vl[fl[x][1]])
+		for x in range(len(fl)): #x starts at 0
+			glVertex3f(vl[fl[x][1]][0], vl[fl[x][1]][1], vl[fl[x][1]][2])
+			glVertex3f(vl[fl[x][2]][0], vl[fl[x][2]][1], vl[fl[x][2]][2])
+			glVertex3f(vl[fl[x][3]][0], vl[fl[x][3]][1], vl[fl[x][3]][2])
 
 
-		glEnd()
+		
 		glPopMatrix()
 		return
 def main():
